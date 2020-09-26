bfd4cc8f0d0edd20ea1d291fbf457ff54366b9cb
rakshit <rakshit@fashiondx.co>
2020-09-26 20:52:44 +0530

    Implement backpropagation

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # tensorflow-cpp-begin
 Trying out tutorils for tensorflow in cpp.
+
+# Rough Work
+1-D => float
+F = x * y
+gradX = dF / dx = y
+gradY = dF / dy = x
+[1/dy1 1/dy2 1/dy3] *	[d(xy1)]
+			[d(xy2)]
+			[d(xy3)]
+
+
+[]  [] < [] ->
+[]  []
+[]
+
+# TODO -
+1) Separate out the code.
+2) Initialization of the grad in Tensor.
+3) BFS instead of DFS
--- main.cpp
+++ main.cpp
@@ -1,91 +1,154 @@
 #include<iostream>
+#include<vector>
+#include<assert.h>
 
 
 using namespace std;
 
 
+template <typename T>
 class Tensor;
 
 
+template <typename T>
 class Operation {
 	public:
-		Tensor* one;
-		Tensor* two;
+		Tensor<T>* one;
+		Tensor<T>* two;
 
-		Operation() {};
+		Operation () {};
 
-		virtual Tensor* forward() {
+		virtual Tensor<T>* forward() {
 			return NULL;
 		}
+
+		virtual void backprop(vector<T> grad) {};
 };
 
 
-class MulOperation : public Operation {
+template <typename T>
+class MulOperation : public Operation<T> {
 	public:
-		MulOperation(Tensor* one, Tensor* two) {
+		MulOperation(Tensor<T>* one, Tensor<T>* two) {
 			this->one = one;
 			this->two = two;
 		}
 
-		Tensor* forward();
+		Tensor<T>* forward();
+
+		void backprop(vector<T> grad);
 };
 
 
-class AddOperation : public Operation {
+template <typename T>
+class AddOperation : public Operation<T> {
 	public:
-		AddOperation(Tensor* one, Tensor* two) {
+		AddOperation(Tensor<T>* one, Tensor<T>* two) {
 			this->one = one;
 			this->two = two;
 		}
 
-		Tensor* forward();
+		Tensor<T>* forward();
+
+		void backprop(vector<T> grad);
 };
 
 
+template <typename T>
 class Tensor {
 	public:
-		float val;
-		Operation* frontOp = NULL;
+		T val;
+		vector<T> grad;
+		Operation<T>* frontOp = NULL;
+		Operation<T>* backOp = NULL;
 
-		Tensor () {};
+		Tensor<T> () {};
 
-		Tensor (float v) {
+		Tensor<T> (T v) {
 			this->val = v;
+			this->grad.push_back(1);
 		}
 
-		// Overload * operator to add two Box objects.
-		Tensor* operator*(const Tensor* two) {
-			this->frontOp = new MulOperation(this, (Tensor*)two);
-			return this->frontOp->forward();
+		Tensor<T>* forward() {
+			Tensor<T>* three = this->frontOp->forward();
+			three->backOp = this->frontOp;
+			return three;
 		}
 
-		// Overload + operator to add two Box objects.
-		Tensor* operator+(const Tensor* two) {
-			this->frontOp = new AddOperation(this, (Tensor*)two);
-			return this->frontOp->forward();
+		void backprop(vector<T> grad) {
+			assert("Shape mismatch" && this->grad.size() == grad.size());
+			for (int i = 0; i < grad.size(); i++) {
+				this->grad[i] *= grad[i];
+			}
+
+			if (this->backOp == NULL) return;
+			this->backOp->backprop(this->grad);
+		}
+
+		// Overload * operator to add two Tensor objects.
+		Tensor<T>* operator*(const Tensor<T>* two) {
+			this->frontOp = new MulOperation<T>(this, (Tensor<T>*)two);
+			return this->forward();
+		}
+
+		// Overload + operator to add two Tensor objects.
+		Tensor<T>* operator+(const Tensor<T>* two) {
+			this->frontOp = new AddOperation<T>(this, (Tensor<T>*)two);
+			return this->forward();
 		}
 };
 
 
-Tensor* MulOperation::forward() {
-	return new Tensor(this->one->val * this->two->val);
+template <typename T>
+Tensor<T>* MulOperation<T>::forward() {
+	return new Tensor<T>(this->one->val * this->two->val);
 }
 
 
-Tensor* AddOperation::forward() {
-	return new Tensor(this->one->val + this->two->val);
+template <typename T>
+Tensor<T>* AddOperation<T>::forward() {
+	return new Tensor<T>(this->one->val + this->two->val);
+}
+
+
+template <typename T>
+void MulOperation<T>::backprop(vector<T> grad) {
+	vector<T> gradOne;
+	vector<T> gradTwo;
+	for (int i = 0; i < grad.size(); i++) {
+		gradOne.push_back(grad[i] * this->two->val);
+		gradTwo.push_back(grad[i] * this->one->val);
+	}
+	this->one->backprop(gradOne);
+	this->two->backprop(gradTwo);
+}
+
+
+template <typename T>
+void AddOperation<T>::backprop(vector<T> grad) {
+	this->one->backprop(grad);
+	this->two->backprop(grad);
 }
 
 
 int main() {
-	Tensor* one = new Tensor(2);
-	Tensor* two = new Tensor(4);
+	Tensor<float>* one = new Tensor<float>(2);
+	Tensor<float>* two = new Tensor<float>(4);
 
-	Tensor* three = (*one) * two;
-	Tensor* four = new Tensor(5);
+	Tensor<float>* three = (*one) * two;
+	Tensor<float>* four = new Tensor<float>(5);
 
-	Tensor* five = (*three) + four;
-	cout << five->val << endl;
+	Tensor<float>* five = (*three) + four;
+
+	vector<float> grad;
+	grad.push_back(1);
+	five->backprop(grad);
+
+	cout << five->grad[0] << endl;
+	cout << four->grad[0] << endl;
+	cout << three->grad[0] << endl;
+	cout << two->grad[0] << endl;
+	cout << one->grad[0] << endl;
 
 	return 0;
 }
